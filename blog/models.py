96e2e1c4f2a2605d0b369a3d41445a0171db8715
SungYong <tjddyddl93@gmail.com>
2021-01-05 14:30:43 +0900

    blog post list

--- blog/models.py
+++ blog/models.py
@@ -9,4 +9,4 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '{} :: {}'.format(self.title, self.author)git
+        return '{} :: {}'.format(self.title, self.author)
