eea5cd3505ccf8d290880b10ca70a3382e1aeaf6
SungYong <tjddyddl93@gmail.com>
2021-01-14 14:37:12 +0900

    post_detail test init

--- blog/tests.py
+++ blog/tests.py
@@ -5,10 +5,27 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your tests here.
 
+def create_post(title, content, author):
+    blog_post = Post.objects.create(
+        title=title,
+        content=content,
+        created=timezone.now(),
+        author=author
+    )
+
+    return blog_post
+
+
+
 class TestView(TestCase):
     def setUp(self):
         self.clinet = Client()
         self.author_000 = User.objects.create(username='test', password='testpassword')
+
+    def check_navbar(self, soup):
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
 
     def test_post_list(self):
         response = self.client.get('/blog/')
@@ -18,18 +35,15 @@ class TestView(TestCase):
         title = soup.title
 
         self.assertEqual(title.text, 'Blog')
-        navbar = soup.find('div', id='navbar')
 
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        self.check_navbar(soup)
 
         self.assertEqual(Post.objects.count(), 0)
         self.assertIn('아직 게시물이 없습니다.', soup.body.text)
 
-        post_000 = Post.objects.create(
+        post_000 = create_post(
             title='The first post',
             content='Hello World, We are the world',
-            created=timezone.now(),
             author=self.author_000,
         )
 
@@ -43,3 +57,24 @@ class TestView(TestCase):
 
         self.assertNotIn('아직 게시물이 없습니다.', body.text)
         self.assertIn(post_000.title, body.text)
+
+    def test_post_detail(self):
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World, We are the world',
+            author=self.author_000,
+        )
+
+        self.assertGreater(Post.objects.count(), 0)
+        post_000_url = post_000.get_absolute_url()
+        self.assertEqual(post_000_url,  '/blog/{}/'.format(post_000.pk))
+
+        response = self.client.get(post_000_url)
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title
+
+        self.assertEqual(title.text, '{} - Blog'.format(post_000.title))
+
+        self.check_navbar(soup)
