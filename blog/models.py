27c9897d3e73e06e69bf718a82854ced5a33126e
SungYong <tjddyddl93@gmail.com>
2021-01-21 16:15:01 +0900

    post detail test bug fixed

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -10,8 +16,11 @@ class Post(models.Model):
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
 
     def get_absolute_url(self):
         return '/blog/{}/'.format(self.pk)
+
--- blog/tests.py
+++ blog/tests.py
@@ -1,19 +1,56 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, Category
 from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your tests here.
 
-def create_post(title, content, author):
+def create_category(name='life', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description
+    )
+
+    return category
+
+def create_post(title, content, author, category=None):
     blog_post = Post.objects.create(
         title=title,
         content=content,
         created=timezone.now(),
-        author=author
+        author=author,
+        category=category,
     )
 
     return blog_post
+
+class TestModel(TestCase):
+    def setUp(self):
+        self.client=Client()
+        self.author_000 = User.objects.create(username='test', password='testpassword')
+
+    def test_category(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World, We are the world',
+            author=self.author_000,
+            category=category
+        )
+
+        self.assertEqual(category.post_set.count(), 1)
+
+    def test_post(self):
+        category = create_category()
+
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World, We are the world',
+            author=self.author_000,
+            category=category
+        )
 
 
 
@@ -27,7 +64,7 @@ class TestView(TestCase):
         self.assertIn('Blog', navbar.text)
         self.assertIn('About me', navbar.text)
 
-    def test_post_list(self):
+    def test_post_list_no_post(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
 
@@ -41,6 +78,7 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 0)
         self.assertIn('아직 게시물이 없습니다.', soup.body.text)
 
+    def test_post_list_with_post(self):
         post_000 = create_post(
             title='The first post',
             content='Hello World, We are the world',
@@ -81,3 +119,11 @@ class TestView(TestCase):
         self.assertEqual(title.text, '{} - Blog'.format(post_000.title))
 
         self.check_navbar(soup)
+
+        body = soup.body
+
+        main_div = body.find('div', id='main_div')
+        self.assertIn(post_000.title, main_div.text)
+        self.assertIn(post_000.author.username, main_div.text)
+
+        self.assertIn(post_000.content, main_div.text)
