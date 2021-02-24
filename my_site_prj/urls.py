d53558c3896ba7b260451e760642b10238099f6a
SungYong <tjddyddl93@gmail.com>
2021-02-24 18:09:48 +0900

    login, logout with aesthetic

--- my_site_prj/urls.py
+++ my_site_prj/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
     path('accounts/', include('allauth.urls')),
